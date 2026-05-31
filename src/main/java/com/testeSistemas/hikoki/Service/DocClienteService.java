6d30d684ca3707c6262958ecc987a10ed5f92343
yoolalw <larakemilyprofissional@gmail.com>
2026-05-31 17:43:27 -0300

    feat/Cripto nos DocClientes

--- src/main/java/com/testeSistemas/hikoki/Service/DocClienteService.java
+++ src/main/java/com/testeSistemas/hikoki/Service/DocClienteService.java
@@ -2,11 +2,10 @@ package com.testeSistemas.hikoki.Service;
 
 import com.testeSistemas.hikoki.Entity.DocCliente;
 import com.testeSistemas.hikoki.Repository.DocClienteRepository;
-import com.testeSistemas.hikoki.Repository.PecaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
+import org.springframework.security.crypto.password.PasswordEncoder; // ← import que faltava
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 import java.util.NoSuchElementException;
 
@@ -16,17 +15,24 @@ public class DocClienteService {
     @Autowired
     private DocClienteRepository docClienteRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public List<DocCliente> listarDocCliente() {
-        Sort sort = Sort.by("nomeCliente").descending();
         return docClienteRepository.findAll();
     }
 
     public DocCliente novoDocCliente(DocCliente docCliente) {
+        String senhaCrip = passwordEncoder.encode(docCliente.getCnpjCliente());
+        docCliente.setCnpjCliente(senhaCrip);
+        senhaCrip = passwordEncoder.encode(docCliente.getTelefoneCliente());
+        docCliente.setTelefoneCliente(senhaCrip);
         return docClienteRepository.save(docCliente);
     }
 
     public void deletarDocCliente(Integer id) {
-        DocCliente docCliente = docClienteRepository.findById(id).orElseThrow(NoSuchElementException::new);
+        DocCliente docCliente = docClienteRepository.findById(id)
+                .orElseThrow(NoSuchElementException::new);
         docClienteRepository.delete(docCliente);
     }
 }
